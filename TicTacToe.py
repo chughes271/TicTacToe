96d31b7b455ebfd1c2f74291900791ceb80bf274
housebox <house@houseboxs-MacBook-Pro.local>
2022-01-15 15:05:14 -0500

    Adding weekly challenges and a teaser file

---
+++ TicTacToe.py
@@ -0,0 +1,12 @@
+
+"""
+Challenges for the week:
+    - Figure out how to represent the game board
+    - Print an individual game board
+    - Create a list of moves and manually represent the game state after each move
+    - Create a system for handling illegal moves (will need to throw errors)
+    - Create a way to take human input to _create_ moves
+    - Play a game of tic tac toe against yourself.
+"""
+
+print("Good luck and may the odds forever be in your favor")
