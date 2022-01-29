627438fb42b02bd403171fba496ac3256b2d1482
chughes271 <chhughes@mtu.edu>
2022-01-29 12:09:59 -0500

    TicTacToe version 2 using functions

--- TicTacToe_2.py
+++ TicTacToe_2.py
@@ -1,46 +1,102 @@
-""" COMMENTS
-    - How many functions are too many functions
-
+"""
+    - Are there better ways to read inputs and make sure it is what you want
+        -instead of a bunch of if statments?
+    -
 
 """
 
 
-import math
-import numpy as np
+def printboard(board):
+    for boardrow in board:
+        print(f'{boardrow[0]}|{boardrow[1]}|{boardrow[2]}')
 
-matrix = np.matrix([[0,0,0],[0,0,0],[0,0,0]])
-displaymatrix = np.matrix([[7,8,9], [4,5,6] ,[1,2,3]])
-
-def ask(playerturn):
-    if math.ceil(playerturn/2) == playerturn/2:
-        ask.playerloc = input("Player 1: choose a location to play.")
-        ask.playerid = "X"
-    elif math.ceil(playerturn/2) != playerturn/2:
-        ask.playerloc = input("PLayer 2: choose a location to play")
-        ask.playerid = "O"
-
-
-def input_check(playerloc):
-    if playerloc == "1" or playerloc == "2" or playerloc == "3" or playerloc == "4" or playerloc == "5" or playerloc == "6" or playerloc == "7" or playerloc == "8" or playerloc == "9":
-        for row in range(2):
-            for col in range(2):
-                if displaymatrix[row][col] == playerloc:
-                    if matrix[row][col] != 0:
-                        print("Location is already filled. Try another location")
-                    else:
-                        matrix[row][col] = ask.playerid
-
+def updateboard(m,n,board,player):
+    if player == 1:
+        board[m][n] = "X"
+        printboard(board)
     else:
-        print("Invalid input. Please choose a valid location to play")
+        board[m][n] = "O"
+        printboard(board)
 
 
-print(displaymatrix)
-print("Above shows the TicTacToe board and locations to play")
+#NEED TO MAKE A WHILE LOOP INSTEAD OF RECALLING FUNCTIONS BC IF BAD INPUT THE RETURNS WILL NOT END AND THEREFORE CAUSE ERRORS
 
+
+def askplayer(player,board):
+    m = 0
+    n = 0
+    loopend = False
+
+    while(loopend == False):
+        if player == 0:
+            inp = inputcheck(input("Player 1: Input row and column values. ex: '1,2' "),player,board)
+        else:
+            inp= inputcheck(input("Player 2: Input row and column values as a tuple. ex: '1,2' "),player,board)
+
+        if inp != False:
+            loopend = True
+            return(inp)
+
+def inputcheck(inp,player,board):
+    #print(inp)
+    if len(inp) == 3:
+        if inp[1] == ",":
+            if inp[0].isnumeric() and inp[2].isnumeric():
+                m = int(inp[0])
+                n = int(inp[2])
+                if (0 <= m <=2) and (0 <= n <=2):
+                    if board[m][n] == "_":
+                        return (m, n)
+                    else:
+                        print("Location already filled. Try again.")
+                        return (False)
+                else:
+                    print("Please input a number 0 thorugh 2")
+                    return(False)
+            else:
+                print("Ensure row and columns are numbers")
+                return(False)
+        else:
+            print("Invalid input. Try again")
+            return(False)
+    else:
+        print("Invalid input. Try again")
+        return(False)
+
+def checkforwin(board):
+    #Checks horizontal wins
+    for row in board:
+        if row[0] == row[1] and row[1] == row[2]:
+            return(row[0])
+    #Checks vertical wins
+    for col in board:
+        if col[0] == col[1] and col[1] == col[2]:
+            return(col[0])
+    #Checks diagonal wins
+    if board[0][0] == board[1][1] and board[1][1] == board[2][2]:
+        return(board[0][0])
+    elif board[0][2] == board[1][1] and board[1][1] == board[2][0]:
+        return(board[0][2])
+
+
+board = [["_","_","_"],
+         ["_","_","_"],
+         ["_","_","_"]]
 
 for k in range(9):
-    ask(k)
-    input_check(ask.playerloc)
+    if ((k) % 2) == 0:
+        player = 0
+    else:
+        player = 1
 
-
-    print(ask.playerloc)
+    m,n = askplayer(player,board)
+    updateboard(m,n,board,player)
+    winner = checkforwin(board)
+    if winner == "X":
+        print("Player 2 wins!")
+        break
+    elif winner == "O":
+        print("Player 1 wins!")
+        break
+    elif k == 8:
+        print("It's a tie!")
