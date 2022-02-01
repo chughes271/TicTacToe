dbda82f8069559334eca2c5a791c35115f45b095
chughes271 <chhughes@mtu.edu>
2022-02-01 15:41:55 -0500

    Version 2 of tictactoe using functions

--- TicTacToe_2.py
+++ TicTacToe_2.py
@@ -1,9 +1,14 @@
 """
     - Are there better ways to read inputs and make sure it is what you want
         -instead of a bunch of if statments?
-    -
+    - Don't know if the way I kept asking (if incorrect input) was the best way.
+        -Ran into problems of function not returning or returning the incorrect info
+    - I think the board is a global var - if it is IDK how else to initiate just in the first loop
+        - Did not want to keep re clearing the board when loop was running
+    - setting up Python for loops confuse me
 
 """
+import numpy
 
 
 def printboard(board):
@@ -66,16 +71,16 @@ def inputcheck(inp,player,board):
 def checkforwin(board):
     #Checks horizontal wins
     for row in board:
-        if row[0] == row[1] and row[1] == row[2]:
+        if row[0] == row[1] and row[1] == row[2] and row[0] != "_":
             return(row[0])
     #Checks vertical wins
-    for col in board:
-        if col[0] == col[1] and col[1] == col[2]:
+    for col in numpy.transpose(board):
+        if col[0] == col[1] and col[1] == col[2]  and col[0] != "_":
             return(col[0])
     #Checks diagonal wins
-    if board[0][0] == board[1][1] and board[1][1] == board[2][2]:
+    if board[0][0] == board[1][1] and board[1][1] == board[2][2] and board[0][0] != "_":
         return(board[0][0])
-    elif board[0][2] == board[1][1] and board[1][1] == board[2][0]:
+    elif board[0][2] == board[1][1] and board[1][1] == board[2][0] and board[0][2] != "_":
         return(board[0][2])
 
 
